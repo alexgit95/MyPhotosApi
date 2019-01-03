b03717fdbe4a09da74d233cbb917f1bfe231443e
Alexandre B <bulbilex89@hotmail.com>
2019-01-03 19:52:37 +0100

    Commit initial avec eclipse

---
+++ src/main/java/com/alex/api/apiPhoto/ApiPhotoApplication.java
@@ -0,0 +1,14 @@
+package main.java.com.alex.api.apiPhoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPhotoApplication.class, args);
+	}
+
+}
+
