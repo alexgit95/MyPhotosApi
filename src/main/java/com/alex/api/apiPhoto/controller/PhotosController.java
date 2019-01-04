ae07b60f1d177a4c82ad891e73005611db65275e
Alexandre B <bulbilex89@hotmail.com>
2019-01-04 23:37:33 +0100

    Modification de l'import base64 pour passer sous maven

--- src/main/java/com/alex/api/apiPhoto/controller/PhotosController.java
+++ src/main/java/com/alex/api/apiPhoto/controller/PhotosController.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Base64;
 import java.util.Date;
 import java.util.List;
 
@@ -23,8 +24,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
-import com.sun.org.apache.xml.internal.security.utils.Base64;
+
+
 
 import main.java.com.alex.api.apiPhoto.model.Photos;
 import main.java.com.alex.api.apiPhoto.services.PhotosRepositoryCustom;
@@ -41,10 +42,10 @@ public class PhotosController {
 	private PhotosRepositoryCustom repositoryPhotos;
 	
 	@RequestMapping( value="/date/{datedebut}/{datefin}", method = RequestMethod.GET)
-	public @ResponseBody List<Photos> getPhotosByDate(@PathVariable String datedebut,@PathVariable String datefin) throws Base64DecodingException, ParseException {
+	public @ResponseBody List<Photos> getPhotosByDate(@PathVariable String datedebut,@PathVariable String datefin) throws ParseException {
 		System.out.println("dateDebut:"+datedebut+", dateFin:"+datefin);
-		datedebut = new String(Base64.decode(datedebut));
-		datefin = new String(Base64.decode(datefin));
+		datedebut = new String(Base64.getDecoder().decode(datedebut));
+		datefin = new String(Base64.getDecoder().decode(datefin));
 		System.out.println("dateDebut:"+datedebut+", dateFin:"+datefin);
 		Date debut=sdf.parse(datedebut);
 		Date fin=sdf.parse(datefin);
