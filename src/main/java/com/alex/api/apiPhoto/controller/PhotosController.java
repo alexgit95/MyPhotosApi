2f161772d12b7f80d0b7051c313548a7b5ad4281
Alex B <you@example.com>
2019-07-01 23:52:15 +0200

    La recuperation de photo fappel le us binairePhoto

--- src/main/java/com/alex/api/apiPhoto/controller/PhotosController.java
+++ src/main/java/com/alex/api/apiPhoto/controller/PhotosController.java
@@ -1,16 +1,16 @@
 package main.java.com.alex.api.apiPhoto.controller;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Base64;
+import java.util.Base64.Encoder;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.IOUtils;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
@@ -23,9 +23,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-
-
+import org.springframework.web.client.RestTemplate;
 
 import main.java.com.alex.api.apiPhoto.model.Photos;
 import main.java.com.alex.api.apiPhoto.services.PhotosRepositoryCustom;
@@ -83,20 +81,52 @@ public class PhotosController {
 	}
 	
 	@RequestMapping(value = "/binary/{id}", method = RequestMethod.GET,produces = MediaType.IMAGE_JPEG_VALUE)
-	public ResponseEntity<byte[]> getImageAsResponseEntity(@PathVariable String id) throws IOException {
-	    Photos findPhotosById = repositoryPhotos.findPhotosById(id);
-	    if(findPhotosById!=null) {
-	    	HttpHeaders headers = new HttpHeaders();
-		    InputStream in = new FileInputStream(new File(findPhotosById.chemin));
-			    byte[] media = IOUtils.toByteArray(in);
-		    headers.setCacheControl(CacheControl.noCache().getHeaderValue());
-		     
-		    ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(media, headers, HttpStatus.OK);
-		    return responseEntity;
-	    }
-		
-		return null;
-		
+
+	public ResponseEntity<byte[]> getImageAsResponseEntity(@PathVariable String id, HttpServletRequest request)
+			throws IOException {
+
+		Photos findPhotosById = repositoryPhotos.findPhotosById(id);
+		if (findPhotosById == null) {
+			return null;
+		}
+		RestTemplate restTemplate = new RestTemplate();
+		Encoder encoder = Base64.getEncoder();
+		String chemin = encoder.encodeToString(findPhotosById.chemin.getBytes());
+		String ip = encoder.encodeToString(request.getRemoteAddr().getBytes());
+		String call = "http://localhost:8282/binaire/" + ip + "/" + chemin;
+		System.out.println(call);
+		ResponseEntity<byte[]> response = restTemplate.getForEntity(call, byte[].class);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setCacheControl(CacheControl.maxAge(1, TimeUnit.DAYS));
+		ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(response.getBody(), headers, HttpStatus.OK);
+
+		return responseEntity;
+
+		/*
+		 * 
+		 * 
+		 * 
+		 * if(findPhotosById!=null) {
+		 * 
+		 * HttpHeaders headers = new HttpHeaders();
+		 * 
+		 * InputStream in = new FileInputStream(new File(findPhotosById.chemin));
+		 * 
+		 * byte[] media = IOUtils.toByteArray(in);
+		 * 
+		 * headers.setCacheControl(CacheControl.noCache().getHeaderValue());
+		 * 
+		 * 
+		 * 
+		 * ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(media, headers,
+		 * HttpStatus.OK);
+		 * 
+		 * return responseEntity;
+		 * 
+		 * }
+		 * 
+		 */
+
 	}
 	
 	
